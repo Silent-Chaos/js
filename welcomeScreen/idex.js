746f0944abbd9fcce41449db897effe5b5ac1afb
SilentChaos <avanishm471@gmail.com>
2023-01-20 17:46:02 +0530

    welcome page

---
+++ welcomeScreen/idex.js
@@ -0,0 +1,8 @@
+let welcomeEl=document.getElementById("welcome-El")
+
+let name="avanish morya"
+let greeting="Hi, Welcome to the Page "
+let myGreeting=greeting+name
+welcomeEl.innerText=myGreeting
+
+
